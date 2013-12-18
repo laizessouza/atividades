281dde041e79a857e7a7539f50ff249ae53c70eb
laizessouza <laize-souza1@hotmail.com>
2013-12-18 10:27:03 -0800

    Create index.php

---
+++ texto/index.php
@@ -0,0 +1,16 @@
+<?php
+
+ 
+$dia = 11;
+$mes = 12;
+$ano = 2013;
+$texto =  "ANIME-SE, HOJE É DIA DE FESTINHA "; 
+
+echo $texto . $dia . "/" . $mes . "/" . $ano;
+echo "<br/>";
+echo "O dia hoje é $dia/$mes/$ano";
+
+
+
+
+?>
