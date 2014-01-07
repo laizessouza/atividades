fbaa716a2168462e8cde4d4cd5fcb36c5afefa67
laizessouza <laize-souza1@hotmail.com>
2014-01-07 12:32:32 -0800

    Create form_cadastrar.php

---
+++ cadastro/form_cadastrar.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Cadastro de pessoas</title>
+    </head>
+    <body>
+        <?php
+            require_once("menu.php");
+        ?>
+        <form action="cadastrar.php" method="post">
+            <input type="text" name="nome" />
+            <input type= "submit" value="Enviar" />
+        </form>
+        
+    </body>
+    
+</html>
