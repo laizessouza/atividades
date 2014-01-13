7acc12073a7028ea733382f8f4e83939fb6b517c
laizessouza <laize-souza1@hotmail.com>
2014-01-13 17:26:41 -0200

    Create campos_produtos.php

---
+++ avalicao_3/campos_produtos.php
@@ -0,0 +1,25 @@
+<input type="text" name="nome" />
+</br>
+<label>Cor: </label>
+<input type="radio" name="cor" value="a" checked="checked" />
+<label>Azul</label>
+<input type="radio" name="cor" value="v" />
+<label>Vermelhor</label>
+<input type="radio" name="cor" value="l" />
+<label>laranja</label>
+<br/>
+<input type="checkbox" name="novo" value="Sim" />
+<label>Produto Novo: </label>
+<br/>
+<label>Prazo de Entrega</label>
+<select name="prazo">
+    <option>5 dias.</option>
+    <option>10 dias.</option>
+    <option>15 dias.</option>
+    <option></option>
+</select>
+<br/>
+<label>Detalhe do Produto: </label>
+<br/>
+<textarea name="detalhe" rows="8" cols="20"></textarea>
+
