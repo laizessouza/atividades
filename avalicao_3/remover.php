59986a57fe7ff02aef698ca94dbc3dfa5d2bc098
laizessouza <laize-souza1@hotmail.com>
2014-01-13 17:31:27 -0200

    Create remover.php

---
+++ avalicao_3/remover.php
@@ -0,0 +1,17 @@
+<?php
+    require_once("menu.php");
+    
+    session_start();
+    if (!isset($_SESSION["cadastros"])){
+        echo "Não existem produtos para remover";
+    }
+    else{
+        $id = $_REQUEST["id"];
+        
+        $cadastros =& $_SESSION["cadastros"];
+        $cadastros[$id] = null;
+        
+        echo "Remoção efetuada com sucesso !";
+        
+    }
+    ?>
